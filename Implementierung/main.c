f1c5ddfe20be34657099c89deed156d3cf92ea1f
RafeekNasrallah <rafeeknasrallah@gmail.com>
2020-07-19 18:43:59 +0200

    added commentes, made code looks better

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -1,5 +1,4 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
@@ -7,63 +6,51 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#include<time.h>
 
 //assembly functions with its args
 extern void peano_meander(u_int64_t degree, u_int64_t *x, u_int64_t *y);
 
 int main(int argc, char** argv) {
+
+    //--------------------------Start-Checking-Right-Input-----------------------------
     // check whether it becomes a number when calling it
     if (argc != 2) {
         printf("add number please\n");
         return 1;
     }
-    //initializing two pointers
-    u_int64_t *x,*y;
-    // turning the number it gets from the console into int
+    
     int n = atoi(argv[1]);
     if (n<1){
         printf("The number must be Positive and greater than 0.\n");
         return 1;
     }
+    //--------------------------End-Checking-for-Right-Input-----------------------------
+
+
+    //---------------------------Start-Main-Part-------------------------------------
+    //initializing two pointers
+    u_int64_t *x,*y;
+    // turning the number it gets from the console into int
     int base = 3;
     double squares = pow(base,(2*n));;
     x =  malloc((squares) * sizeof(u_int64_t));
     y =  malloc((squares) * sizeof(u_int64_t)); 
-   
-   // -----------------------Start-Zeitmessung----------------------------------------
-    struct timespec start;
-    struct timespec end;
 
-    if(clock_gettime(CLOCK_MONOTONIC, &start)){
-    printf("ERROR: start time");
-    return 1;
-    }
+    peano_meander(n,x,y);
+    //------------------------------End-Main-Part-------------------------------------
 
-    	peano_meander(n,x,y);
 
-    if(clock_gettime(CLOCK_MONOTONIC, &end)){
-        printf("ERROR: end time");
-        return 1;
-    }
-    
-
-    double time = end.tv_sec - start.tv_sec + 1e-9 *
-        (end.tv_nsec - start.tv_nsec);
-    
-    printf("the time is: %f \n", time);
-
-    // -----------------------End-Zeitmessung----------------------------------------   
-    // printing the values of the pointer
-    
+    //------------------------------Start-SVG-------------------------------------------
     FILE *file = fopen("graph.svg", "w");
-    if (file == NULL)
-    {
+
+    if (file == NULL){
     printf("Error\n");
     exit(1);
     }
+
     double sqrtpoints = sqrt(squares) - 1; // number of points on each axis
     fprintf(file,"<svg viewBox=\"800 -1000 1100 1100\" xmlns=\"http://www.w3.org/2000/svg\">");
+    
     for (int i = 0;i<squares-1;i++)
     {
     u_int64_t tempx1,tempy1,tempx2,tempy2;
@@ -73,9 +60,11 @@ int main(int argc, char** argv) {
     tempy2 = y[i+1]*(900/sqrtpoints) * -1;
     fprintf(file, "<line x1=\"%ld\" y1=\"%ld\" x2=\"%ld\" y2=\"%ld\" stroke=\"black\" transform=\"scale (-1, 1)\" transform-origin=\"center\" />\n",tempx1,tempy1,tempx2,tempy2);
     }
-   
+
     fprintf(file,"</svg>");
     fclose(file);
+    //------------------------------End-SVG-------------------------------------------
+
     free(x);
     free(y); 
     return 0;
